9a9f6150a7b04a150c0413ac0f55789ca7a9c4e6
eazyhomez <info@eazyhomez.com>
2015-04-30 17:30:12 +0530

    Prep: ------ 1. Get inner wall segments 2. Calculate distance (perpendicular) of a point from a line 3. Get inner wall segments of a room (Living) - Tested [packaged 2] 4. Get all FWS of a room (Living) below the given elevation (7' : door height) 5. Get free inner wall segments of a room with length >= 3'
    Code
    ----
    A. Get all FWS in room (Living)

--- src/com/eteks/test/PhoenixPCS.java
+++ src/com/eteks/test/PhoenixPCS.java
@@ -28,46 +28,44 @@ public class PhoenixPCS extends Plugin
 
 		public List<String> furnIds = new ArrayList<String>();
 		public List<float[][]> furnRects = new ArrayList<float[][]>();
-		public List<float[][]> furnRectsBloated = new ArrayList<float[][]>();
-		
-		public List<HomePieceOfFurniture> furnList = new ArrayList<HomePieceOfFurniture>();
-		
+		public List<float[][]> furnRectsBloated = new ArrayList<float[][]>();	
 		public List<Float> furnElevs = new ArrayList<Float>();
 		public List<Float> furnHeights = new ArrayList<Float>();
+		public List<float[][]> furnRectsAccess = new ArrayList<float[][]>();
 		
-		public List<String> markBoxName = new ArrayList<String>();
-
-		public int MARKBOX_COUNT = 8;
-		public HomePieceOfFurniture[] markBoxes = new HomePieceOfFurniture[MARKBOX_COUNT];		
-
-		public int markerIndx = 0;
+		public List<HomePieceOfFurniture> furnList = new ArrayList<HomePieceOfFurniture>();
 
 		public List<String> wallIds = new ArrayList<String>();
 		public List<float[][]> wallRects = new ArrayList<float[][]>();
 		public List<Float> wallThicks = new ArrayList<Float>();
+		
+		public int MARKBOX_COUNT = 8;
+		public List<String> markBoxName = new ArrayList<String>();
+		public HomePieceOfFurniture[] markBoxes = new HomePieceOfFurniture[MARKBOX_COUNT];		
+
+		public int markerIndx = 0;
 
 		public float FURN_TOLERANCE = 0.51f;
 		public float ROOM_TOLERANCE = 0.51f;
 		public float WALL_TOLERANCE = 0.1f;
-
+		public float ORIENTATION_TOLERANCE = 0.05f;
+		public float SLOPE_TOLERANCE = 0.01f;
+		public float tolerance = 0.5f; 	// 5 mm
+		public float VALID_INNERWALL_TOLERANCE = 0.5f;	// 5mm
+		
 		public float FURNITURE_BLOAT_SIZE = 5.0f;	// 2cm
 
-		public float VALID_INNERWALL_TOLERANCE = 0.5f;	// 5mm
-
-		public float SLOPE_TOLERANCE = 0.01f;
 		public float INFINITY = 10000.0f; 
-		public float tolerance = 0.5f; 	// 5 mm
-
-		public boolean bShowMarkerInter = false;
-		public boolean bShowMarker = true;
 
 		public float CONV_IN_CM = 2.54f;
 		public float CONV_FT_CM = (12.0f * CONV_IN_CM);
 
 		public float VALID_RS_LENGTH = (3.0f * CONV_FT_CM);
-
 		public float DOOR_ELEVATION = (7.0f * CONV_FT_CM);
 
+		public boolean bShowMarkerInter = false;
+		public boolean bShowMarker = true;
+		
 		// ======================= CLASSES ======================= //
 
 		public class Points
@@ -198,93 +196,7 @@ public class PhoenixPCS extends Plugin
 
 				markBoxes = getMarkerBoxes();
 
-				// 1. Get inner wall segments ----------- //
-				//List<WallSegement> innerWSList = getInnerWalls();
-
-				// ================================================== //
-
-				// 2. Calculate distance (perpendicular) of a point from a line ----------- //				
-				/*
-				getLivingRoom();
-				float[][] livinRect = livingRoom.getPoints();
-
-				List<WallSegement> innerWSList = getInnerWalls();
-
-				int count = 0;
-
-				for(int l = 0; l < livinRect.length; l++)
-				{
-					count++;
-
-					Points startP = new Points(livinRect[l][0], livinRect[l][1]);
-					Points endP = null;
-
-					if(l == (livinRect.length - 1))
-						endP = new Points(livinRect[0][0], livinRect[0][1]);
-					else
-						endP = new Points(livinRect[l+1][0], livinRect[l+1][1]);
-
-					Points midP = new Points(((startP.x + endP.x)/2.0f),((startP.y + endP.y)/2.0f));
-					LineSegement rs = new LineSegement(startP, endP);
-
-					for(WallSegement ws : innerWSList)
-					{						
-						LineSegement ls = new LineSegement(ws);
-
-						if(isParallel(rs, ls, tolerance))
-						{							
-							float dist = calcDistancePointLine(midP, ls, tolerance);
-
-							if(dist <= VALID_INNERWALL_TOLERANCE)
-							{
-								putMarkers(midP, (count % MARKBOX_COUNT));
-
-								Points midWS = new Points(((ws.startP.x + ws.endP.x)/2.0f),((ws.startP.y + ws.endP.y)/2.0f));
-								putMarkers(midWS, (count % MARKBOX_COUNT));
-							}
-						}
-					}
-				}			
-				*/
-
-				// ================================================== //
-				/*
-				// 3. Get inner wall segments of a room (Living) ----------- //	
-				
-				getLivingRoom();
-				float[][] livinRect = livingRoom.getPoints();
-
-				List<WallSegement> innerWSList = getInnerWalls();
-
-				getValidInnerWallSegmentsOfRoom(innerWSList, livinRect, tolerance);			
-				*/
-				
-				// ================================================== //
-				
-				// 4. Get all FWS of a room (Living) below the given elevation (7' : door height)  ----------- //	
-				/*
-				getLivingRoom();
-				float[][] livinRect = livingRoom.getPoints();
-				List<WallSegement> innerWSList = getInnerWalls();
-				
-				List<WallSegement> validWSList = getValidInnerWallSegmentsOfRoom(innerWSList, livinRect, tolerance);	
-				
-				List<WallSegement> fWSList = calcFreeWallIntersectionsBelowElev(validWSList, DOOR_ELEVATION, livingRoom, 1.0f);
-				
-				for(WallSegement freeWS : fWSList)
-				{
-					if(freeWS.len >= VALID_RS_LENGTH)
-					{
-						// Marker
-						Points midWS = new Points(((freeWS.startP.x + freeWS.endP.x)/2.0f),((freeWS.startP.y + freeWS.endP.y)/2.0f));
-						putMarkers(midWS, 1);
-					}
-				}
-				*/
-				
-				// ================================================== //
-				
-				// 5. Get free inner wall segments of a room with length >= 3'   ----------- //	
+				// A. Get all FWS in room (Living) --------- //
 				
 				getLivingRoom();
 				float[][] livinRect = livingRoom.getPoints();
@@ -742,6 +654,7 @@ public class PhoenixPCS extends Plugin
 
 			return freeWallSegList;
 		}
+		
 		// ======================= INIT FUNCTIONS ======================= //
 
 		public void getLivingRoom()
@@ -841,6 +754,197 @@ public class PhoenixPCS extends Plugin
 
 		// ======================= UTIL FUNCTIONS ======================= //
 
+		public void placeFurnParallelToWall(LineSegement ws, HomePieceOfFurniture furn, Points furnCoords)
+		{
+			FurnLoc furnLoc = new FurnLoc();
+			float furnAngle =  calcWallAngles(ws);
+			
+			furnLoc.w = furn.getWidth();
+			furnLoc.ang = furnAngle;			
+			furnLoc.p = furnCoords;	
+			
+			placeFurnItem(furn, furnLoc);
+		}
+		
+		public void placeFurnItem(HomePieceOfFurniture inFurn, FurnLoc fLoc)
+		{
+			HomePieceOfFurniture outFurn = inFurn;
+			outFurn.setName(inFurn.getName());
+			outFurn.setWidth(fLoc.w);
+			outFurn.setAngle(fLoc.ang);
+			outFurn.setX(fLoc.p.x);
+			outFurn.setY(fLoc.p.y);
+			
+			home.addPieceOfFurniture(outFurn);
+		}
+		
+		public float calcWallAngles(LineSegement ws)
+		{
+			float retAngle = 0.0f;
+			
+			float wsAngle =  (float) Math.atan((Math.abs(ws.endP.y - ws.startP.y)) / (Math.abs(ws.endP.x - ws.startP.x))); 
+			
+			Points p = new Points((ws.startP.x - ws.endP.x), (ws.startP.y - ws.endP.y));
+			int qIndx = getQuadrantInfo(p);
+			
+			if(qIndx == 1)
+				retAngle = wsAngle;
+			else if(qIndx == 2)
+				retAngle = (float)(Math.PI) - wsAngle;
+			else if(qIndx == 3)
+				retAngle = (float)(Math.PI) + wsAngle;
+			else if(qIndx == 4)
+				retAngle = (float)(2.0f*Math.PI) - wsAngle;
+			
+			return retAngle;
+		}
+		
+		public int getQuadrantInfo(Points p)
+		{
+			int qIndx = 0;
+			
+			if((p.x > 0.0f) && (p.y > 0.0f))
+				qIndx = 1;
+			else if((p.x < 0.0f) && (p.y > 0.0f))
+				qIndx = 2;
+			else if((p.x < 0.0f) && (p.y < 0.0f))
+				qIndx = 3;
+			else if((p.x > 0.0f) && (p.y < 0.0f))
+				qIndx = 4;
+			
+			return qIndx;
+		}
+		
+		public void chkFurnOrient(HomePieceOfFurniture furn, LineSegement ws)
+		{			
+			float[][] fRect = furn.getPoints();
+			Points furnBottMid = new Points(((fRect[2][0] + fRect[3][0]) / 2),  ((fRect[2][1] + fRect[3][1]) / 2));
+			
+			Points wsMid = new Points(((ws.startP.x + ws.endP.x) / 2),  ((ws.startP.y + ws.endP.y) / 2));
+			
+			float dist = calcDistance(furnBottMid, wsMid);
+			//JOptionPane.showMessageDialog(null, "dist : " + dist);
+			
+			if(dist > ORIENTATION_TOLERANCE)
+			{
+				furn.setAngle((float)Math.PI);
+				//JOptionPane.showMessageDialog(null, "180 rotation");
+			}
+		}
+		
+		public boolean checkIntersectWithAllFurns(HomePieceOfFurniture hpf, boolean bAddAccessibility)
+		{
+			boolean bIntersects = false;
+			
+			for(int x = 0 ; x < furnIds.size(); x++)
+			{
+				if(!hpf.getName().equalsIgnoreCase(furnIds.get(x)) && !(furnIds.get(x).toLowerCase().startsWith("diningrect")))
+				{	
+					float[][] refFurnRect = furnRects.get(x);
+					
+					for(int f = 0; f < refFurnRect.length; f++)
+					{
+						Points startLine = new Points(refFurnRect[f][0], refFurnRect[f][1]);
+						
+						Points endLine = null;
+						
+						if(f == (refFurnRect.length - 1))
+							endLine = new Points(refFurnRect[0][0], refFurnRect[0][1]);
+						else
+							endLine = new Points(refFurnRect[f+1][0], refFurnRect[f+1][1]);				
+						
+						LineSegement ls = new LineSegement(startLine, endLine);
+						
+						// For Accessibility check
+						List<Intersect> interList = new ArrayList<Intersect>();
+						
+						if(bAddAccessibility)
+							interList = checkIntersectAccessibility(ls, hpf.getName());
+						else
+							interList = checkIntersect(ls, hpf.getName());
+						
+						for(Intersect inter : interList)
+						{
+							if(inter != null)
+							{
+								bIntersects = checkPointInBetween(inter.p, ls.startP, ls.endP, FURN_TOLERANCE);
+								
+								if(bIntersects)
+									break;
+							}
+							//putMarkers(inter.p, 3);
+						}
+					}
+					
+					if(bIntersects)
+						break;
+				}
+				
+				if(bIntersects)
+					break;
+			}
+			
+			return bIntersects;
+		}
+		
+		public List<Intersect> checkIntersectAccessibility(LineSegement r, String furnId)
+		{
+			List<Intersect> interList = new ArrayList<Intersect>();
+			
+			Intersect inter = null;
+			int indx = -1;
+			
+			if((indx = furnIds.indexOf(furnId)) > -1)
+			{ 				
+				float[][] fRect = furnRectsAccess.get(indx);
+						
+				if(fRect.length == 2)
+				{
+					LineSegement l1 = new LineSegement((new Points(fRect[0][0], fRect[0][1])) , (new Points(fRect[1][0], fRect[1][1])));
+					
+					inter = getIntersectPoint(r, l1);				
+					if(inter.dist < INFINITY)
+						interList.add(inter);
+					
+					//debug += ("1. " + inter.p.x + "," + inter.p.y + " -> " + inter.dist + "\n");
+				}
+				else if(fRect.length == 4)
+				{
+					LineSegement l1 = new LineSegement((new Points(fRect[0][0], fRect[0][1])) , (new Points(fRect[1][0], fRect[1][1])));
+					LineSegement l2 = new LineSegement((new Points(fRect[1][0], fRect[1][1])) , (new Points(fRect[2][0], fRect[2][1])));
+					LineSegement l3 = new LineSegement((new Points(fRect[2][0], fRect[2][1])) , (new Points(fRect[3][0], fRect[3][1])));
+					LineSegement l4 = new LineSegement((new Points(fRect[3][0], fRect[3][1])) , (new Points(fRect[0][0], fRect[0][1])));
+					
+					inter = getIntersectPoint(r, l1);				
+					if(inter.dist < INFINITY)
+						interList.add(inter);
+					
+					//debug += ("1. " + inter.p.x + "," + inter.p.y + " -> " + inter.dist + "\n");
+					
+					inter = getIntersectPoint(r, l2);				
+					if(inter.dist < INFINITY)
+						interList.add(inter);
+					
+					//debug += ("2. " + inter.p.x + "," + inter.p.y + " -> " + inter.dist + "\n");
+					
+					inter = getIntersectPoint(r, l3);
+					if(inter.dist < INFINITY)
+						interList.add(inter);
+					
+					//debug += ("3. " + inter.p.x + "," + inter.p.y + " -> " + inter.dist + "\n");
+					
+					inter = getIntersectPoint(r, l4);
+					if(inter.dist < INFINITY)
+						interList.add(inter);
+					
+					//debug += ("4. " + inter.p.x + "," + inter.p.y + " -> " + inter.dist + "\n");
+				}
+				//JOptionPane.showMessageDialog(null, debug);					
+			}
+			
+			return interList;
+		}
+		
 		public List<Intersect> checkIntersect(LineSegement r, String furnId)
 		{
 			List<Intersect> interList = new ArrayList<Intersect>();
